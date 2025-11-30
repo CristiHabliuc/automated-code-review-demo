39e9368f37ac4b62345545059eb1a3d6d71aea4d
Cristi Habliuc <cristi.habliuc@gmail.com>
2025-11-30 17:13:06 +0100

    hello world project

---
+++ NoteTaker/NoteTaker/NoteTakerApp.swift
@@ -0,0 +1,17 @@
+//
+//  NoteTakerApp.swift
+//  NoteTaker
+//
+//  Created by Cristi Habliuc on 30/11/25.
+//
+
+import SwiftUI
+
+@main
+struct NoteTakerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ NoteTaker/NoteTakerTests/NoteTakerTests.swift
@@ -0,0 +1,17 @@
+//
+//  NoteTakerTests.swift
+//  NoteTakerTests
+//
+//  Created by Cristi Habliuc on 30/11/25.
+//
+
+import Testing
+@testable import NoteTaker
+
+struct NoteTakerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
